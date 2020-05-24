a2c4a79107bae53455cda9c61eac656d1f4d9277
Sulaiman <sulaiman_666@apps.ipb.ac.id>
2020-05-24 15:57:29 +0700

    Object Oriented Programming

---
+++ ObjectOrientationProgramming/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        // Variable to initiate the WordTool.class
+        WordTool wt = new WordTool();
+
+        // Variable to store given text
+        String word = "Learning Java Object Oriented Programming";
+
+        // Printing code to check WordTool.class is working or not
+        System.out.println("Counting given text " + word);
+        // Using WordCount function from WordTool.class
+        System.out.println("Total words: " + wt.WordCount(word));
+        //Using FullCount function with space from WordTool.class
+        System.out.println("Total letter with space: " + wt.FullCount(word, true));
+        //Using FullCount function without space from WordTool.class
+        System.out.println("Total letter without space: " + wt.FullCount(word, false));
+    }
+}
---
+++ ObjectOrientationProgramming/src/WordTool.java
@@ -0,0 +1,37 @@
+public class WordTool {
+    // Function to initiate the class if you want to access/use it from other class
+    WordTool() {};
+
+    // Function to count letter from given word
+    public int WordCount (String s){
+        // Variable to count letter
+        int count = 0;
+
+        // Check if given string is empty/null, if not then execute given code
+        if(!(s == null || s.isEmpty())){
+            // Using split method from string class to merge the given word by deleting whitespace
+            String[] word = s.split("\\s+");
+            count = word.length;
+        }
+        return count;
+    }
+
+    // Function to count letter from given word but white space/space included
+    public int FullCount (String s, boolean Space){
+        // Variable that used to count how many letter in given word
+        int count = 0;
+
+        // Check if the word/string given is empty or not
+        if(!(s == null || s.isEmpty())){
+            // If condition if text with space
+            if (Space){
+                // Counting the whole text with space
+                count = s.length();
+            } else{
+                // Counting the text without space by replacing it
+                count = s.replace(" ", "").length();
+            }
+        }
+        return count;
+    }
+}
